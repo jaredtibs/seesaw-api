17d769ae5afaa45db034de5d371e39cb0c6388b8
Jared Tibshraeny <jmtibs@gmail.com>
2016-08-03 23:20:24 -0700

    tweaks to authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+
+  devise_for :users, skip: :all
+  devise_scope :user do
+    post 'registrations'  => 'registrations#create'
+    post 'sessions'       => 'sessions#create'
+    get  'sessions'       => 'sessions#show'
+  end
+
 end
