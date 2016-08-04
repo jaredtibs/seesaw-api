432ce75d3fa5147d0e9c22c8e95b786aa71a391a
Jared Tibshraeny <jmtibs@gmail.com>
2016-08-03 23:13:48 -0700

    users table, start of authentication

--- config/application.rb
+++ config/application.rb
@@ -18,13 +18,9 @@ Bundler.require(*Rails.groups)
 
 module Dropnote
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.autoload_paths << Rails.root.join('lib')
+    config.active_record.raise_in_transactional_callbacks = true
   end
 end
