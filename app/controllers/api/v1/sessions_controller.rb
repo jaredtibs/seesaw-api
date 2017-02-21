7c684e88cfa859a715ff29d43b3ae91cb00ccabe
Jared Tibshraeny <jmtibs@gmail.com>
2017-02-20 17:33:09 -0800

    remove byebug

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::SessionsController < Api::V1::BaseController
   before_action :authenticate_user!, except: :create
 
   def create
-    byebug
     @current_user = User.find_by email: params[:username]
 
     if @current_user and @current_user.valid_password?(params[:password])
