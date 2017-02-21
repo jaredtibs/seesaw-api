2233965f8a653c8199955034ecf94ff0f685ff6f
Jared Tibshraeny <jmtibs@gmail.com>
2017-02-20 17:26:12 -0800

    include serialized users in login and register responses

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,12 +2,13 @@ class Api::V1::SessionsController < Api::V1::BaseController
   before_action :authenticate_user!, except: :create
 
   def create
-    @current_user = User.find_by username: params[:username]
+    byebug
+    @current_user = User.find_by email: params[:username]
 
     if @current_user and @current_user.valid_password?(params[:password])
       token = UserService.generate_token_for @current_user
       sign_in @current_user
-      render json: {token: token}, status: :created
+      render json: {token: token, user: JSONAPI::Serializer.serialize(@current_user)}, status: :created
     else
       unauthorized
     end
