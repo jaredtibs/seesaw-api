e7d14ede879003ff0d0263396ea86d718e499895
Jared Tibshraeny <jmtibs@gmail.com>
2017-04-30 20:15:01 -0700

    notification serializer

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,3 +1,12 @@
 class Notification < ApplicationRecord
   belongs_to :user
+
+  def serialized_user
+    if user
+      ActiveModelSerializers::SerializableResource.new(user, serializer: UserSerializer)
+    else
+      {}
+    end
+  end
+
 end
--- app/serializers/notification_serializer.rb
+++ app/serializers/notification_serializer.rb
@@ -1,9 +1,17 @@
 class NotificationSerializer < ActiveModel::Serializer
 
   attributes(
+    :user,
+    :body,
+    :checked
   )
 
   def notification
     @notification ||= object
   end
+
+  def user
+    notification.serialized_user
+  end
+
 end
